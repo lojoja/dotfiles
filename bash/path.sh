4bda293f8a20b7fb3915b2e06f6b31481d425dbc
lojoja <dev@lojoja.com>
2017-04-20 18:29:19 -0400

    fix sqlite path

--- bash/path.sh
+++ bash/path.sh
@@ -1,7 +1,7 @@
 # dotfiles/bash/path.sh
 
 PATH="/usr/local/sbin:$PATH"
-PATH="/usr/local/opt/sqlite/share/bin:$PATH"
+PATH="/usr/local/opt/sqlite/bin:$PATH"
 PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 PATH="/usr/local/opt/findutils/libexec/gnubin:$PATH"
