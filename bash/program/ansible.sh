e98758e62afd38748638667b236aa96a72f04812
lojoja <github@lojoja.com>
2017-08-03 15:48:10 -0400

    update ansible SSH options

--- bash/program/ansible.sh
+++ bash/program/ansible.sh
@@ -4,19 +4,16 @@
 export ANSIBLE_REMOTE_TEMP='~/.ansible/tmp'
 export ANSIBLE_LOCAL_TEMP='/tmp/ansible'
 export ANSIBLE_VAULT_PASSWORD_FILE="$HOME/.ansible-vault"
-export ANSIBLE_MANAGED=
 
 # Connection
-export ANSIBLE_USE_PERSISTENT_CONNECTIONS='True'
-export ANSIBLE_SSH_ARGS='-C -o ControlMaster=auto -o ControlPersist=15m -o ForwardAgent=yes'
 export ANSIBLE_SSH_CONTROL_PATH='%(directory)s/ssh-%%h-%%p-%%r'
 export ANSIBLE_SSH_CONTROL_PATH_DIR="$ANSIBLE_LOCAL_TEMP"
-#export ANSIBLE_SSH_PIPELINING='True'
+export ANSIBLE_SSH_PIPELINING=1
 
 # Display
-export ANSIBLE_FORCE_COLOR='True'
-export ANSIBLE_DISPLAY_SKIPPED_HOSTS='True'
-export ANSIBLE_RETRY_FILES_ENABLED='False'
+export ANSIBLE_FORCE_COLOR=1
+export ANSIBLE_DISPLAY_SKIPPED_HOSTS=1
+export ANSIBLE_RETRY_FILES_ENABLED=0
 
 # Plugins
 export ANSIBLE_SQUASH_ACTIONS=''
