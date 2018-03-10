5f0860f37cbc01c2df0c9592fb440fd1c41507db
lojoja <github@lojoja.com>
2018-03-10 08:00:59 -0500

    Homebrew changed python and python3 path again
    /usr/local/bin/python = python2
    /usr/local/bin/python3 = python3

--- bash/manpath.sh
+++ bash/manpath.sh
@@ -3,7 +3,7 @@
 MANPATH="/usr/share/man"
 MANPATH="/usr/local/share/man:$MANPATH"
 MANPATH="/usr/local/opt/sqlite/share/man:$MANPATH"
-MANPATH="/usr/local/opt/python@2/share/man:$MANPATH"
+#MANPATH="/usr/local/opt/python@2/share/man:$MANPATH"
 MANPATH="/usr/local/opt/gnu-tar/libexec/gnuman:$MANPATH"
 MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
 MANPATH="/usr/local/opt/findutils/libexec/gnuman:$MANPATH"
