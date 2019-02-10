d9f914de2be8b8de5920f6538ceed728b3e37907
lojoja <github@lojoja.com>
2019-02-10 04:27:42 -0500

    Add ffmpeg helper functions

---
+++ bash/program/ffmpeg.sh
@@ -0,0 +1,17 @@
+# dotfiles/bash/program/ffmpeg.sh
+
+if [[ $DOTFILES_INIT_MAC -eq 1 ]]
+then
+  if hash ffmpeg &>/dev/null
+  then
+    function ffmpegSetMovieGPS() {
+      local lat="$1"
+      local lon="$2"
+      local file="$3"
+      local extension="${file##*.}"
+      local filename="${file%.*}"
+
+      ffmpeg -i $file -metadata location=${lat}${lon} -metadata location-eng=${lat}${lon} -codec copy "${filename}_tagged.${extension}"
+    }
+  fi
+fi
