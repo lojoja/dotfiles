c191fdeffcbd8d7bf244ce82e07860ce35f7c444
lojoja <dev@lojoja.com>
2022-01-07 07:48:28 -0500

    Add upgradeVenv function

--- bash/program/python.sh
+++ bash/program/python.sh
@@ -6,3 +6,9 @@ export PYTHONSTARTUP="$DOTFILES/python/startup.py"
 alias py='python'
 alias py2='python'
 alias py3='python3'
+
+function upgradeVenv() {
+  for venv in /usr/local/opt/{lojoja,snldev}/*; do
+    /usr/local/bin/python3 -m venv --upgrade $venv
+  done
+}
