ce95127256e94ff043e5427a5e64f7df0259c643
lojoja <dev@lojoja.com>
2022-09-23 09:29:36 -0400

    Use env var for Poetry cache dir; tilde is not expanded from the config file

--- bash/program/poetry.sh
+++ bash/program/poetry.sh
@@ -2,6 +2,7 @@
 
 if hash poetry &>/dev/null
 then
+  export POETRY_CACHE_DIR="$HOME/Library/Caches/pypoetry"
   export POETRY_CONFIG_DIR="$HOME/.config/poetry"
   export POETRY_CONFIG_FILE="$POETRY_CONFIG_DIR/config.toml"
 fi
