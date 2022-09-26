e801d023eb75648b4313e9008a3687ef953c8eac
lojoja <dev@lojoja.com>
2022-09-26 04:10:55 -0400

    Set github host dynamically in pip custom install

--- bash/program/pip.sh
+++ bash/program/pip.sh
@@ -5,17 +5,25 @@ export PIP_CONFIG_FILE="$DOTFILES/pip/pip.conf"
 function pipPriv() {
   local user=$1
   local repo=$2
-  local url="git+ssh://git@github.com:/$user/$repo.git"
+
+  if [[ $user = 'snldev' ]]
+  then
+    local host='gh-snldev'
+  else
+    local host='gh'
+  fi
+
+  local url="git+ssh://$host:/$user/$repo.git"
   local venv="/usr/local/opt/$user/$repo"
 
   if [[ -d "$venv" ]]
   then
-    /usr/local/bin/python3 -m venv --upgrade $venv
+    /usr/local/bin/python3 -m venv --upgrade "$venv"
   else
-    /usr/local/bin/python3 -m venv --copies $venv
+    /usr/local/bin/python3 -m venv --copies "$venv"
   fi
 
-  $venv/bin/pip install $url --no-cache-dir --no-binary :all:
+  "$venv/bin/pip" install "$url" --no-cache-dir --no-binary :all:
 
   if ! [[ -r "/usr/local/bin/$repo" ]]
   then
