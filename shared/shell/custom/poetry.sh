05ec82be0ea6737a518a5231fddadb895b856c02
lojoja <dev@lojoja.com>
2022-12-25 10:17:29 -0500

    Update Poetry environment vars with gitea package repo

--- shared/shell/custom/poetry.sh
+++ shared/shell/custom/poetry.sh
@@ -6,4 +6,8 @@ then
   export POETRY_CACHE_DIR="$HOME/Library/Caches/pypoetry"
   export POETRY_CONFIG_DIR="$DOTFILES_INSTALLED_CONFIG_PATH/poetry"
   export POETRY_CONFIG_FILE="$POETRY_CONFIG_DIR/config.toml"
+  export POETRY_REPOSITORIES_LOJOJA_URL="https://gitea.lojoja.com/api/packages/lojoja/pypi"
+  export POETRY_REPOSITORIES_SNLDEV_URL="https://gitea.lojoja.com/api/packages/snldev/pypi"
+  export POETRY_CERTIFICATES_LOJOJA_CERT="$DOTFILES_SHARED_PATH/lojojaCerts.pem"
+  export POETRY_CERTIFICATES_SNLDEV_CERT="$DOTFILES_SHARED_PATH/lojojaCerts.pem"
 fi
