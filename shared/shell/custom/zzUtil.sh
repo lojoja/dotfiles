b2f5e8b8a5827008fb5baba3b131781fd830ebcb
lojoja <dev@lojoja.com>
2022-12-27 20:04:23 -0500

    set local var scope outside of loop

--- shared/shell/custom/zzUtil.sh
+++ shared/shell/custom/zzUtil.sh
@@ -6,26 +6,35 @@ function utilAdd() {
 
   local args=("${@}")
   local python="$MPBIN/python3.10"
+  local owner
+  local package
+  local venv
+  local venv_bin
+  local pip
+  local pip_args
+  local index
+  local file
+  local re
 
   for arg in "${args[@]}"
   do
-    local owner="${arg%/*}"
-    local package="${arg#*/}"
-    local venv="$HBOPT/util/$package"
-    local venv_bin="$venv/bin"
-    local pip="$venv_bin/pip"
+    owner="${arg%/*}"
+    package="${arg#*/}"
+    venv="$HBOPT/util/$package"
+    venv_bin="$venv/bin"
+    pip="$venv_bin/pip"
 
     if [[ $owner = 'lojoja' ]]
     then
-      local index="$GITEA_PYPI_PACKAGE_INDEX_LOJOJA"
+      index="$GITEA_PYPI_PACKAGE_INDEX_LOJOJA"
     elif [[ $owner = 'snldev' ]]
       then
-        local index="$GITEA_PYPI_PACKAGE_INDEX_SNLDEV"
+        index="$GITEA_PYPI_PACKAGE_INDEX_SNLDEV"
     else
       printf "Error: Unknown user %s\n" "$owner" && return 1
     fi
 
-    local pip_args="--extra-index-url=$index"
+    pip_args="--extra-index-url=$index"
 
     if [[ ! -d $venv ]]
     then
@@ -41,9 +50,9 @@ function utilAdd() {
 
     for file_path in "$venv_bin"/*
     do
-      local file
+      file
       file=$(basename "$file_path")
-      local re="^([Aa]ctivate|pip|python).*"
+      re="^([Aa]ctivate|pip|python).*"
 
       if [[ ! $file =~ $re ]]
       then
@@ -57,11 +66,15 @@ function utilDel() {
   # Uninstall private cli utils by passing one or more utility names to this function.
 
   local args=("${@}")
+  local venv
+  local venv_bin
+  local file
+  local re
 
   for arg in "${args[@]}"
   do
-    local venv="$HBOPT/util/$arg"
-    local venv_bin="$venv/bin"
+    venv="$HBOPT/util/$arg"
+    venv_bin="$venv/bin"
 
     if [[ -d $venv ]]
     then
@@ -69,9 +82,9 @@ function utilDel() {
 
       for file_path in "$venv_bin"/*
       do
-        local file
+        file
         file=$(basename "$file_path")
-        local re="([Aa]ctivate|pip|python).*"
+        re="([Aa]ctivate|pip|python).*"
 
         if [[ ! $file =~ $re ]]
         then
