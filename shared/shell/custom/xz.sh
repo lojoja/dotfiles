ea23c9051e33c0de6013a46c79d3d47d640f1a66
lojoja <dev@lojoja.com>
2023-03-15 15:06:09 -0400

    Limit XZ thread count

--- shared/shell/custom/xz.sh
+++ shared/shell/custom/xz.sh
@@ -1,5 +1,7 @@
 # dotfiles/shared/shell/custom/xz.sh
 # shellcheck shell=bash
 
-export XZ_DEFAULTS='-9 --threads=0'
-export XZ_OPT='-9 --threads=0'
+threads="$(($(nproc) - 1))"  # Lower thread count to account for default memory limit
+
+export XZ_DEFAULTS='-9 --threads='$threads
+export XZ_OPT='-9 --threads='$threads
