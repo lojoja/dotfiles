946f705538c065eb63cde16807949b4aaaad6032
lojoja <dev@lojoja.com>
2023-06-27 10:29:41 -0400

    Combine common dotfiles script command routines; set and validate vault password locally without ansible; reorganize inventory and set host automatically at runtime

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -4,24 +4,99 @@
 
 PROGRAM_NAME=dotfiles
 REPOSITORY=https://github.com/lojoja/dotfiles
-REPOSITORY_PATH="${0:A:h:h}"
+REPOSITORY_PATH="${0:A:h}"
 LEGACY_REPOSITORY_PATH="$HOME/.dotfiles"
 
-###############
-# Environment #
-###############
+############
+# Commands #
+############
+
+typeset -A OPT=(
+  'help' '-h, --help'
+  'test' '-t, --test'
+)
+
+typeset -A OPT_DESC=(
+  'help' 'Show this message and exit'
+  'test' 'Test the ansible playbooks without altering the system'
+)
+
+typeset -A CMD=(
+  'main' ''
+  'install' 'install'
+  'password' 'password'
+  'uninstall' 'uninstall'
+  'update' 'update'
+)
+
+typeset -A CMD_DESC=(
+  'main' "Manage $PROGRAM_NAME on this system"
+  'install' "Install $PROGRAM_NAME"
+  'password' "Set/validate the $PROGRAM_NAME vault password"
+  'uninstall' "Uninstall $PROGRAM_NAME"
+  'update' "Update $PROGRAM_NAME"
+)
+
+typeset -A CMD_SUBCMD=(
+  'main' "$CMD[install]::$CMD_DESC[install]::$CMD[password]::$CMD_DESC[password]::$CMD[uninstall]::$CMD_DESC[uninstall]::$CMD[update]::$CMD_DESC[update]"
+  'install' ''
+  'password' ''
+  'uninstall' ''
+  'update' ''
+)
+
+typeset -A CMD_OPTS=(
+  'main' "$OPT[help]::$OPT_DESC[help]"
+  'install' "$OPT[help]::$OPT_DESC[help]::$OPT[test]::$OPT_DESC[test]"
+  'password' "$OPT[help]::$OPT_DESC[help]"
+  'uninstall' "$OPT[help]::$OPT_DESC[help]::$OPT[test]::$OPT_DESC[test]"
+  'update' "$OPT[help]::$OPT_DESC[help]::$OPT[test]::$OPT_DESC[test]"
+)
+
+typeset -A CMD_MSG_END=(
+  'install' "$PROGRAM_NAME installed"
+  'password' "$PROGRAM_NAME vault password validated"
+  'uninstall' "$PROGRAM_NAME uninstalled"
+  'update' "$PROGRAM_NAME updated"
+)
+
+typeset -A CMD_MSG_FAIL=(
+  'install' "$PROGRAM_NAME installation failed"
+  'password' "$PROGRAM_NAME vault password validation failed"
+  'uninstall' "$PROGRAM_NAME uninstall failed"
+  'update' "$PROGRAM_NAME update failed"
+)
+
+typeset -A CMD_MSG_START=(
+  'install' "Installing $PROGRAM_NAME"
+  'password' "Checking $PROGRAM_NAME vault password"
+  'uninstall' "Uninstalling $PROGRAM_NAME"
+  'update' "Updating $PROGRAM_NAME"
+)
+
+###########
+# Ansible #
+###########
 
 unset ANSIBLE_LOCAL_TEMP
 unset ANSIBLE_REMOTE_TEMP
 unset ANSIBLE_SQUASH_ACTIONS
 unset ANSIBLE_SSH_CONTROL_PATH
 unset ANSIBLE_SSH_CONTROL_PATH_DIR
-unset ANSIBLE_VAULT_PASSWORD_FILE
 
 export ANSIBLE_DISPLAY_SKIPPED_HOSTS=0
 export ANSIBLE_FORCE_COLOR=1
 export ANSIBLE_RETRY_FILES_ENABLED=0
 export ANSIBLE_SSH_PIPELINING=1
+export ANSIBLE_VAULT_PASSWORD_FILE="$REPOSITORY_PATH/.ansible-vault"
+
+# A mapping of command names to ansible playbooks. Multiple playbooks are separated by "::".
+# When multiple playbooks are specified they are run in the order listed.
+typeset -A PLAYBOOKS=(
+  $CMD[install] 'uninstall_legacy.yml'
+  $CMD[uninstall] 'uninstall.yml'
+  $CMD[update] 'update.yml'
+)
 
 ##########
 # Output #
@@ -74,40 +149,34 @@ function warn() {
 
 # Print the command help and exit.
 #
-# $1 - The active command name, if different than the root command name.
-# $2 - The active command description.
-# $3 - The command options as a 'name::description' delimited string. Optional.
-# $4 - The command subcommands as a 'name::description' delimited string. Optional.
+# $1 - The command name.
 function showHelp() {
-  validateArgCount "$0" $# 2 4
-  local command="${1:-""}"
-  local description="$2"
-  local options="${3:-""}"
-  local commands="${4:-""}"
+  validateArgCount "$0" $# 1 1
+  local name="$1"
   local usage=(
     "$STYLES[blue]$STYLES[bold]Usage:$STYLES[reset] $PROGRAM_NAME"
-    "${command:+"$STYLES[green]$STYLES[bold]$command$STYLES[reset]"}"
-    "${options:+"$STYLES[cyan]$STYLES[bold][OPTIONS]$STYLES[reset]"}"
-    "${commands:+"$STYLES[red]$STYLES[bold]COMMAND$STYLES[reset]"}"
-    "${commands:+"$STYLES[yellow]$STYLES[bold][ARGS...]$STYLES[reset]"}"
+    "${CMD[$name]:+"$STYLES[green]$STYLES[bold]$CMD[$name]$STYLES[reset]"}"
+    "${CMD_OPTS[$name]:+"$STYLES[cyan]$STYLES[bold][OPTIONS]$STYLES[reset]"}"
+    "${CMD_SUBCMD[$name]:+"$STYLES[red]$STYLES[bold]COMMAND$STYLES[reset]"}"
+    "${CMD_SUBCMD[$name]:+"$STYLES[yellow]$STYLES[bold][ARGS...]$STYLES[reset]"}"
   )
 
   print "$usage" | tr -s ' '  # collapse double-spaces for empty items
   print
-  print "  $description"
+  print "  $CMD_DESC[$name]"
 
-  if [[ ! -z $options ]]
+  if [[ ! -z $CMD_OPTS[$name] ]]
   then
     print
     print "$STYLES[cyan]$STYLES[bold]Options:$STYLES[reset]"
-    print -aC 2 -- ${(@s/::/)options} | sed 's/^/  /'
+    print -aC 2 -- ${(@s/::/)CMD_OPTS[$name]} | sed 's/^/  /'
   fi
 
-  if [[ ! -z $commands ]]
+  if [[ ! -z $CMD_SUBCMD[$name] ]]
   then
     print
     print "$STYLES[red]$STYLES[bold]Commands:$STYLES[reset]"
-    print -aC 2 -- ${(@s/::/)commands} | sed 's/^/  /'
+    print -aC 2 -- ${(@s/::/)CMD_SUBCMD[$name]} | sed 's/^/  /'
   fi
 
   quit
@@ -141,30 +210,6 @@ function quit() {
 # Utility Functions #
 #####################
 
-# Format item pairs as a '::' delimited string.
-#
-# $1..n - Alternating key and value pairs (e.g., `joined=$(joinItems 'foo' 'foo val' 'bar' 'bar val')`).
-function joinItems() {
-  validateArgCountEven "$0" $# 2
-  print -n -- ${(j/::/)@}
-}
-
-# Run an ansible playbook.
-#
-# $1 - The name of the playbook to run.
-# $2 - Run playbook in check mode. Set to 1 to use check mode. Default is 0.
-function runPlaybook() {
-  validateArgCount "$0" $# 1 2
-  local playbook="$1" check="${2:-'0'}"
-
-  if [[ $check == 1 ]]
-  then
-    ansible-playbook --check "$playbook"
-  else
-    ansible-playbook "$playbook"
-  fi
-}
-
 # Validate argument count.
 #
 # $1 - The calling command or function name.
@@ -178,141 +223,103 @@ function validateArgCount() {
   (( max >= 0 && count > max )) && die "($caller) Too many arguments"
 }
 
-# Validate argument count and assert the count is even.
+###########
+# Program #
+###########
+
+# Run the program.
 #
-# $1 - The calling command or function name.
-# $2 - The number of arguments received.
-# $3 - The minimum number of arguments required, inclusive. Set to -1 for no limit.
-# $4 - The maximum number of arguments required, inclusive. Set to -1 for no limit.
-function validateArgCountEven() {
-  validateArgCount "$@"
-  local caller="$1" count=$2
-
-  (( count%2 == 0 )) || die "($caller) Expected an even number of arguments"
-}
-
-############
-# Commands #
-############
-
-OPT_HELP_FLAG='-h, --help'
-OPT_HELP_DESC='Show this message and exit'
-
-OPT_TEST_FLAG='-t, --test'
-OPT_TEST_DESC='Test the command without altering the system'
-
-CMD_INSTALL_NAME='install'
-CMD_INSTALL_DESC="Install $PROGRAM_NAME"
-CMD_INSTALL_OPTS="$(joinItems "$OPT_HELP_FLAG" "$OPT_HELP_DESC" "$OPT_TEST_FLAG" "$OPT_TEST_DESC")"
-
-CMD_UNINSTALL_NAME='uninstall'
-CMD_UNINSTALL_DESC="Uninstall $PROGRAM_NAME"
-CMD_UNINSTALL_OPTS="$(joinItems "$OPT_HELP_FLAG" "$OPT_HELP_DESC" "$OPT_TEST_FLAG" "$OPT_TEST_DESC")"
-
-CMD_UPDATE_NAME='update'
-CMD_UPDATE_DESC='Check for and install available updates'
-CMD_UPDATE_OPTS="$(joinItems "$OPT_HELP_FLAG" "$OPT_HELP_DESC" "$OPT_TEST_FLAG" "$OPT_TEST_DESC")"
-
-CMD_MAIN_NAME=''
-CMD_MAIN_DESC="Manage lojoja's dotfiles on this system"
-CMD_MAIN_CMDS="$(joinItems \
-  "$CMD_INSTALL_NAME" "$CMD_INSTALL_DESC" \
-  "$CMD_UNINSTALL_NAME" "$CMD_UNINSTALL_DESC" \
-  "$CMD_UPDATE_NAME" "$CMD_UPDATE_DESC" \
-)"
-CMD_MAIN_OPTS="$(joinItems "$OPT_HELP_FLAG" "$OPT_HELP_DESC")"
-
-
+# $1 - The command or option flag.
+# $2 - When $1 is a command name, an option flag for the command. Optional.
 function main() {
   [[ ${$(uname):l} != 'darwin' ]] && die "$PROGRAM_NAME requires macOS"
   [[ ${EUID:-${UID}} == '0' ]] && die "$PROGRAM_NAME can not be run as root"
 
-  validateArgCount "$0" $# 1 -1
+  validateArgCount "$0" $# 1 2
+
+  local name='main'
 
   case $1 in
-    -h|--help)  showHelp "$CMD_MAIN_NAME" "$CMD_MAIN_DESC" "$CMD_MAIN_OPTS" "$CMD_MAIN_CMDS";;
-    install)    install "${@:2}";;
-    uninstall)  uninstall "${@:2}";;
-    update)     update "${@:2}";;
-    -*)         die "Unknown option $1";;
-    *)          die "Unknown command $1";;
+    -h|--help)                showHelp "$name";;
+    password)                 shift; checkVaultPassword "$@";;
+    install|uninstall|update) name="$1"; shift; runPlaybooks "$name" "$@";;
+    -*)                       die "Unknown option $1";;
+    *)                        die "Unknown command $1";;
   esac
 
   quit
 }
 
-function install() {
+# Set and validate the ansible vault password.
+#
+# $1 - The command option flag (optional).
+function checkVaultPassword() {
   validateArgCount "$0" $# 0 1
-  local check=0 playbooks=('uninstall_legacy.yml' 'vault_password.yml')
+  local name='password' vaultPassword
 
   case $1 in
-    -h|--help)  showHelp "$CMD_INSTALL_NAME" "$CMD_INSTALL_DESC" "$CMD_INSTALL_OPTS";;
-    -d|--debug) check=1;;
+    -h|--help)  showHelp "$name";;
+    '')         ;;
     -*)         die "Unknown option $1";;
-    *)          die "Unknown command $1";;
+    *)          die "Unknown argument $1";;
   esac
 
-  info 'Installing dotfiles'
+  info "$CMD_MSG_START[$name]"
 
-  for playbook in $playbooks
-  do
-    if ! runPlaybook "$playbook" $check
-    then
-      die "Installation failed ($playbook)"
-    fi
-  done
+  if ! [[ -f $ANSIBLE_VAULT_PASSWORD_FILE ]]
+  then
+    while true
+    do
+      print >&2
+      read -s "vaultPassword?$STYLES[magenta]$STYLES[bold]Enter vault password:$STYLES[reset] "
+      [[ $vaultPassword != $'\n' && $vaultPassword != $'\r' ]] && print >&2
+      [[ $vaultPassword == *[![:space:]]* ]] && break
 
-  ok 'dotfiles installed'
-  quit
+      warn 'Vault password cannot be blank'
+    done
+
+    print "$vaultPassword" > "$ANSIBLE_VAULT_PASSWORD_FILE"
+
+  fi
+
+  if ! ansible-vault decrypt --output - tests/vaultTest.yml &>/dev/null
+  then
+    rm "$ANSIBLE_VAULT_PASSWORD_FILE" &>/dev/null
+    die "$CMD_MSG_FAIL[$name]"
+  fi
+
+  ok "$CMD_MSG_END[$name]"
 }
 
-function uninstall() {
-  validateArgCount "$0" $# 0 1
-  local check=0 playbooks=()
+# Run the ansible playbooks for the designated command.
+#
+# $1 - The command name.
+# $2 - The command option flag (optional).
+function runPlaybooks() {
+  validateArgCount "$0" $# 1 2
+  local name="$1" check=''
 
-  case $1 in
-    -h|--help)  showHelp "$CMD_UNINSTALL_NAME" "$CMD_UNINSTALL_DESC" "$CMD_UNINSTALL_OPTS";;
-    -d|--debug) check=1;;
-    -*)         die "Unknown option $1";;
-    *)          die "Unknown command $1";;
+  case $2 in
+    -h|--help)  showHelp "$name";;
+    -t|--test)  check='--check';;
+    '')         ;;
+    -*)         die "Unknown option $2";;
+    *)          die "Unknown argument $2";;
   esac
 
-  info 'Uninstalling dotfiles'
+  checkVaultPassword
 
-  for playbook in $playbooks
+  info "$CMD_MSG_START[$name]"
+
+  for playbook in ${(@s/::/)PLAYBOOKS[$name]}
   do
-    if ! runPlaybook "$playbook" $check
+    if ! ansible-playbook --limit "$(hostname)" $check "$playbook"
     then
-      die "Uninstall failed ($playbook)"
+      die "$CMD_MSG_FAIL[$name]"
     fi
   done
 
-  ok 'dotfiles uninstalled'
-  quit
-}
-
-function update() {
-  validateArgCount "$0" $# 0 1
-  local check=0 playbooks=()
-
-  case $1 in
-    -h|--help)  showHelp "$CMD_UPDATE_NAME" "$CMD_UPDATE_DESC" "$CMD_UPDATE_OPTS";;
-    -d|--debug) check=1;;
-    -*)         die "Unknown option $1";;
-    *)          die "Unknown command $1";;
-  esac
-
-  info 'Updating dotfiles'
-
-  for playbook in $playbooks
-  do
-    if ! runPlaybook "$playbook" $check
-    then
-      die "Update failed ($playbook)"
-    fi
-  done
-
-  ok 'dotfiles updated'
+  ok "$CMD_MSG_END[$name]"
   quit
 }
 
